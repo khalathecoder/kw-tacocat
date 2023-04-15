c5f43ac82d283dd4645f1319319a60884e28fdaf
Khala Wright <khalawright5@gmail.com>
2023-04-14 21:28:32 -0400

    added no spaces

--- js/site.js
+++ js/site.js
@@ -3,28 +3,39 @@ function getValues() {
   // decide what to do with it
   let userInput = document.getElementById("message").value;
 
-  let reversedInput = checkForPalindrome(userInput);
+  let reversedInput = checkForPalindrome(userInput.replaceAll(' ', ''));
 
   displayResults(reversedInput);
-  // you are now calling the function to line 26
+  // you are now calling the function
 }
 
 function checkForPalindrome(string) {
-//   let reversedMessage = "";
+
   let strLength = string.length;
 
   for (let index = 0; index < strLength / 2; index++) {
-    if (string[index] !== string[strLength - 1 - index]); {
+    if (string[index] !== string[strLength - 1 - index]) {
         return false;
-    }
+    }; 
   }
-  return true;
+  return "Your phrase reversed is:" ;
 }
 
-function displayResults(reversedMessage) {
+function displayResults(reversedInput) {
   // show the string on page aka view function
-  document.getElementById("msg").textContent = reversedMessage;
-  document.getElementById("alert").classList.remove("d-none");
+  if(reversedInput) {
+    document.getElementById("msg").textContent = reversedInput;
+  } else {
+    document.getElementById("msg2").textContent = reversedInput;
+  }
+  
+  if(reversedInput){
+    document.getElementById("alert").classList.remove("d-none");
+    document.getElementById("alerttwo").classList.add("d-none");
+  } else {
+    document.getElementById("alerttwo").classList.remove("d-none");
+    document.getElementById("alert").classList.add("d-none");
+  }
 }
 
 // Swal.fire({
